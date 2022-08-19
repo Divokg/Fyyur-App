92a832d5fc07491ca4948f40eec8fbccd566b4d4
Samuel Ndungu <ndungusam1@gmail.com>
2022-08-19 15:47:21 +0300

    import models

--- app.py
+++ app.py
@@ -12,14 +12,22 @@ import logging
 from logging import Formatter, FileHandler
 from flask_wtf import Form
 from forms import *
+from flask_migrate import Migrate
+from models import db, Venue, Artist, Show
+from config import SQLALCHEMY_DATABASE_URI
 #----------------------------------------------------------------------------#
 # App Config.
 #----------------------------------------------------------------------------#
 
 app = Flask(__name__)
+app.config ['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URI
 moment = Moment(app)
 app.config.from_object('config')
 db = SQLAlchemy(app)
+from models import db, Venue, Artist, Show
+db.init_app(app)
+migrate = Migrate(app, db)
+migrate.init_app(app)
 
 # TODO: connect to a local postgresql database
 
@@ -27,35 +35,7 @@ db = SQLAlchemy(app)
 # Models.
 #----------------------------------------------------------------------------#
 
-class Venue(db.Model):
-    __tablename__ = 'Venue'
 
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    city = db.Column(db.String(120))
-    state = db.Column(db.String(120))
-    address = db.Column(db.String(120))
-    phone = db.Column(db.String(120))
-    image_link = db.Column(db.String(500))
-    facebook_link = db.Column(db.String(120))
-
-    # TODO: implement any missing fields, as a database migration using Flask-Migrate
-
-class Artist(db.Model):
-    __tablename__ = 'Artist'
-
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    city = db.Column(db.String(120))
-    state = db.Column(db.String(120))
-    phone = db.Column(db.String(120))
-    genres = db.Column(db.String(120))
-    image_link = db.Column(db.String(500))
-    facebook_link = db.Column(db.String(120))
-
-    # TODO: implement any missing fields, as a database migration using Flask-Migrate
-
-# TODO Implement Show and Artist models, and complete all model relationships and properties, as a database migration.
 
 #----------------------------------------------------------------------------#
 # Filters.
